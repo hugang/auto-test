70d6ff9019c4c99a7295d8b232d871d4a9307b02
hugang <hugang0825@gmail.com>
2023-07-10 21:56:16 +0900

    add test case

--- src/test/java/io/hugang/echo/EchoTest.java
+++ src/test/java/io/hugang/echo/EchoTest.java
@@ -1,4 +1,14 @@
 package io.hugang.echo;
 
+import io.hugang.BasicExecutor;
+import io.hugang.open.OpenTest;
+import org.junit.Test;
+
+import java.util.Objects;
+
 public class EchoTest {
+    @Test
+    public void testAddSelectionXlsx() {
+        new BasicExecutor().execute("xlsx", Objects.requireNonNull(OpenTest.class.getClassLoader().getResource("echo/echo.xlsx")).getPath());
+    }
 }
