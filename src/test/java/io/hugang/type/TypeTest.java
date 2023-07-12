b3a044a390d27704c343faf47b1e81b246db4bf9
hugang <hugang0825@gmail.com>
2023-07-13 06:13:06 +0900

    add test case

--- src/test/java/io/hugang/type/TypeTest.java
+++ src/test/java/io/hugang/type/TypeTest.java
@@ -1,4 +1,19 @@
 package io.hugang.type;
 
+import java.util.Objects;
+
+import org.junit.Test;
+
+import io.hugang.BasicExecutor;
+import io.hugang.open.OpenTest;
+
 public class TypeTest {
+    @Test
+    public void testTypeXlsx() {
+        new BasicExecutor().execute("xlsx", Objects.requireNonNull(OpenTest.class.getClassLoader().getResource("type/type.xlsx")).getPath());
+    }
+    @Test
+    public void testTypeErrorXlsx() {
+        new BasicExecutor().execute("xlsx", Objects.requireNonNull(OpenTest.class.getClassLoader().getResource("type/typeError.xlsx")).getPath());
+    }
 }
