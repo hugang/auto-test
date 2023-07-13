e88a43922ae0fb51a4dc2aa451ea9c95571c4f4d
hugang <hugang0825@gmail.com>
2023-07-13 09:20:20 +0900

    add test case

--- src/test/java/io/hugang/screenshot/ScreenshotTest.java
+++ src/test/java/io/hugang/screenshot/ScreenshotTest.java
@@ -1,4 +1,11 @@
 package io.hugang.screenshot;
 
+import io.hugang.util.Utils;
+import org.junit.Test;
+
 public class ScreenshotTest {
+    @Test
+    public void testScreenshot() {
+        Utils.execute("xlsx", "screenshot/screenshot.xlsx");
+    }
 }
