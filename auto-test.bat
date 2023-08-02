c180535f628ad5ebba333aa00d04278c82628e33
hugang <hugang0825@gmail.com>
2023-08-02 11:11:58 +0900

    bug fix

--- auto-test.bat
+++ auto-test.bat
@@ -1,4 +1,6 @@
 @echo off
 set JAVA_HOME=%USERPROFILE%\.jdks\corretto-11.0.19
+set AUTO_TEST_HOME=C:/projs/auto-test
 set PATH=%PATH%;%JAVA_HOME%\bin
-java -cp "C:/projs/auto-test/libs/*" io.hugang.RunAutoTest %*
+cd %AUTO_TEST_HOME%
+java -cp %AUTO_TEST_HOME%"/libs/*" io.hugang.RunAutoTest %*
