1b99cd5fa499cce507c0132c57377523a3b37f7d
hugang <hugang0825@gmail.com>
2024-01-24 17:23:55 +0900

    update read properties command.

--- src/main/java/io/hugang/execute/ext/ReadPropertiesCommand.java
+++ src/main/java/io/hugang/execute/ext/ReadPropertiesCommand.java
@@ -4,6 +4,9 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import cn.hutool.setting.Setting;
+import cn.hutool.setting.SettingUtil;
+import io.hugang.CommandExecuteException;
 import io.hugang.execute.Command;
 
 /**
@@ -16,6 +19,7 @@ public class ReadPropertiesCommand extends Command {
 
     public static final String KEY_FILE = "value";
     public static final String VALUE_TYPE_JSON = "json";
+    public static final String VALUE_TYPE_PROPERTIES = "properties";
 
     public ReadPropertiesCommand(String command, String target, String value) {
         super(command, target, value);
@@ -30,11 +34,17 @@ public class ReadPropertiesCommand extends Command {
     public boolean _execute() {
         String type = this.getTarget();
         String file = this.getDictStr(KEY_FILE, this.getValue());
+        String filePath = this.getFilePath(file);
 
         if (VALUE_TYPE_JSON.equals(type)) {
-            String filePath = this.getFilePath(file);
             JSONObject json = (JSONObject) JSONUtil.readJSON(FileUtil.file(filePath), CharsetUtil.CHARSET_UTF_8);
             json.forEach(this::setVariable);
+        } else if (VALUE_TYPE_PROPERTIES.equals(type)) {
+            // read a properties file
+            Setting setting = SettingUtil.get(file);
+            setting.forEach(this::setVariable);
+        } else {
+            throw new CommandExecuteException("not support type: " + type);
         }
 
         return true;
