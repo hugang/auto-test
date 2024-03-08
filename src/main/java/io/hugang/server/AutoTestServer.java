6bbb0c8e5e4c555665e37ef5027f0d4b1600cd29
hugang <hugang0825@gmail.com>
2024-03-08 14:55:24 +0900

    update pom.xml and add home page for remote access.

--- src/main/java/io/hugang/server/AutoTestServer.java
+++ src/main/java/io/hugang/server/AutoTestServer.java
@@ -1,8 +1,7 @@
 package io.hugang.server;
 
-import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.FileTypeUtil;
 import cn.hutool.core.net.multipart.UploadFile;
-import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.http.server.SimpleServer;
 import cn.hutool.json.JSONUtil;
@@ -16,7 +15,17 @@ public class AutoTestServer {
 
     public static void main(String[] args) {
         SimpleServer server = HttpUtil.createServer(9191);
-        server
+        server.addAction("/", (request, response) -> {
+                    response.setContentType("text/html; charset=utf-8");
+                    response.write("""
+                            <form method="post" action="/remote" enctype="multipart/form-data">
+                                <label for="file">file</label><input type="file" name="file" id="file"><br>
+                                <label for="testcases">testcases</label><input type="text" name="testcases" id="testcases"><br>
+                                <input type="submit" value="submit"><br>
+                            </form>
+                            """);
+                    response.close();
+                })
                 // run test case from local pc, the test case store in the work folder
                 // e.g. http://localhost:9191/local?testcases=1&path=src/test/resources/recorder/recorder.xlsx
                 .addAction("/local", (request, response) -> {
@@ -38,13 +47,25 @@ public class AutoTestServer {
                         UploadFile file = request.getMultipart().getFile("file");
                         File tempFile = new File(System.currentTimeMillis() + "_" + file.getFileName());
                         file.write(tempFile);
-                        if (file.getFileName().endsWith(".csv") || file.getFileName().endsWith(".json")) {
-                            log.info(FileUtil.readString(tempFile, CharsetUtil.defaultCharset()));
+                        String fileType = FileTypeUtil.getType(tempFile);
+                        log.info("file type: {}", fileType);
+                        // only support xlsx, csv and json file
+                        if (!("zip".equals(fileType) && tempFile.getName().endsWith("xlsm"))
+                                && !"xlsx".equals(fileType) && !"csv".equals(fileType) && !"json".equals(fileType)) {
+                            response.write("not support file type: " + fileType);
+                            response.close();
+                            return;
                         }
                         String path = tempFile.getAbsolutePath();
                         String testCases = request.getMultipart().getParam("testcases");
-                        String mode = request.getMultipart().getParam("mode");
-                        mode = mode == null ? "xlsx" : mode;
+                        String mode;
+                        if ("json".equals(fileType)) {
+                            mode = "json";
+                        } else if ("csv".equals(fileType)) {
+                            mode = "csv";
+                        } else {
+                            mode = "xlsx";
+                        }
                         response.write(JSONUtil.toJsonStr(new BasicExecutor().execute(mode, path, testCases)));
                     } catch (Exception ex) {
                         log.error(ex);
