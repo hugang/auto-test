8a67718b3953ec639abce6c10adefac006b811c6
hugang <hugang0825@gmail.com>
2025-08-06 11:43:33 +0900

    Refactor LogCommand to use OriginalCommand and simplify execution method

--- src/main/java/io/hugang/execute/ext/LogCommand.java
+++ src/main/java/io/hugang/execute/ext/LogCommand.java
@@ -2,19 +2,24 @@ package io.hugang.execute.ext;
 
 import io.hugang.annotation.ReportCommand;
 import io.hugang.annotation.WebCommand;
-import io.hugang.exceptions.CommandExecuteException;
+import io.hugang.bean.OriginalCommand;
 import io.hugang.execute.Command;
 
 @WebCommand
 @ReportCommand
 public class LogCommand extends Command {
+    public LogCommand(OriginalCommand originalCommand) {
+        super(originalCommand);
+    }
+
     @Override
     public String getCommand() {
         return "log";
     }
 
     @Override
-    public boolean _execute() throws CommandExecuteException {
+    public boolean _execute() {
         return true;
+
     }
 }
